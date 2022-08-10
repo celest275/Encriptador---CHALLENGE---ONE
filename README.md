369b1b939ff3ff3c6c5cbcb19af54a556f4bfd13
Celeste <49965033+celest275@users.noreply.github.com>
2022-08-10 16:16:14 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Encriptador - CHALLENGE ONE
+Este es el primer challen del programa ONE 
+El proposito es crear una página donde se pueda ingresar  un mensaje, encriptarlo y desencriptar, dicho mensaje no puede poseer caracteres especiales,  tildes o mayusculas.
---
+++ js/funcionamiento.js
@@ -0,0 +1,29 @@
+function encriptarMensaje() {
+    if(document.getElementById("inputTexto").value.match(/^[a-z ]*$/)){
+
+        const encriptar = document.getElementById('inputTexto').value;
+        const encriptado = encriptar.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
+        document.getElementById("texto2").value = encriptado
+        document.getElementById("copiar").style.display = "block";
+        document.getElementById("texto").style.display = "none";
+        document.getElementById("ilustracion").style.display = "none";
+        document.getElementById("detective").style.display = "block";
+
+    }else{
+        alert("Solo se permiten letras minusculas, sin caracteres especiales, y sin acentos.")
+    }
+    
+}
+
+function desencriptarMensaje(){
+    const desencriptar= document.getElementById ("inputTexto").value;
+    const desencriptado= desencriptar.replace(/ai/gi, 'a').replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
+    document.getElementById("texto2").value = desencriptado
+}
+
+function copiar(){
+    var copyText = document.getElementById("texto2");
+    copyText.select();
+    document.execCommand("copy");
+}
+
